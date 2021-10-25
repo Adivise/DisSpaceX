5e53030b0c5a805fb4beaee1b2d079c71b2d91c6
Nanotect <61177761+Adivise@users.noreply.github.com>
2021-10-26 02:38:34 +0700

    remove msg.edit volume not working!

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -9,7 +9,6 @@ module.exports = {
         aliases: ["vol"]
     },
     run: async (client, message, args) => {
-        const msg = await message.channel.send("Processing.....");
         const { channel } = message.member.voice;
         if (!channel) return message.channel.send("You need to be in a voice channel to play music.");
 
@@ -18,11 +17,11 @@ module.exports = {
         if (!permissions.has("SPEAK")) return message.channel.send("I cannot connect to your voice channel, make sure I have permission to!");
 
         const queue = client.distube.getQueue(message)
-            if (!queue) msg.edit(`There is nothing in the queue right now!`)
-            
+            if (!queue) return message.channel.send(`There is nothing in the queue right now!`)
+
         const volume = parseInt(args[0])
-            if (isNaN(volume)) msg.edit(`Please provide a number between 1 and 100`)
-            if (Number(volume) < 1 || Number(volume) > 100) msg.edit(`Please provide a number between 1 and 100`)
+            if (isNaN(volume)) return message.channel.send(`Please provide a number between 1 and 100`)
+            if (Number(volume) < 1 || Number(volume) > 100) return message.channel.send(`Please provide a number between 1 and 100`)
 
         queue.setVolume(volume)
 
@@ -30,7 +29,7 @@ module.exports = {
             .setColor("#000001")
             .setDescription(`\`🔊\` | **Change volume to:** \`${volume}\``);
 
-        msg.edit('', embed);
+        message.channel.send('', embed);
 
     }
 };
