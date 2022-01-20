7db68899280f033bbfccfda0ad9ff5d66d39b7ed
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-01-20 17:05:22 +0700

    Remove voiceStateUpdate.js

---
+++ disspace.js
@@ -0,0 +1,56 @@
+const { Intents, Client, Collection } = require("discord.js");
+const { DisTube } = require('distube');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+const { SpotifyPlugin } = require('@distube/spotify');
+
+class MainClient extends Client {
+    constructor() {
+       super({
+            shards: "auto",
+            allowedMentions: {
+            parse: ["roles", "users", "everyone"],
+            repliedUser: false
+        },
+        intents: [ 
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        ],
+    });
+
+process.on('unhandledRejection', error => console.log(error));
+process.on('uncaughtException', error => console.log(error));
+
+this.config = require('./settings/config.js');
+this.prefix = this.config.PREFIX;
+this.owner = this.config.OWNER_ID;
+if(!this.token) this.token = this.config.TOKEN;
+
+const client = this;
+
+this.distube = new DisTube(client, {
+  searchSongs: 0,
+  searchCooldown: 30,
+  leaveOnEmpty: this.config.LEAVE_EMPTY,
+  emptyCooldown: this.config.EMPTY_LEAVE,
+  leaveOnFinish: this.config.LEAVE_FINISH,
+  leaveOnStop: this.config.LEAVE_STOP,
+  plugins: [new SoundCloudPlugin(), new SpotifyPlugin()],
+  ytdlOptions: {
+    highWaterMark: 1 << 24,
+    quality: 'highestaudio'
+  },
+});
+
+["aliases", "commands"].forEach(x => client[x] = new Collection());
+["loadCommands", "loadEvents", "loadDistube"].forEach(x => require(`./handlers/${x}`)(client));
+
+
+    }
+        connect() {
+            return super.login(this.token);
+    };
+};
+module.exports = MainClient;
--- index.js
+++ index.js
@@ -1,46 +1,7 @@
-const { Intents, Client, Collection } = require("discord.js");
-const { DisTube } = require('distube');
-const { SoundCloudPlugin } = require('@distube/soundcloud');
-const { SpotifyPlugin } = require('@distube/spotify');
 
-const client = new Client({
-    shards: "auto",
-    allowedMentions: {
-      parse: ["roles", "users", "everyone"],
-      repliedUser: false
-    },
-    intents: [ 
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    ],
-});
+const MainClient = require("./disspace");
+const client = new MainClient();
 
-process.on('unhandledRejection', error => console.log(error));
-process.on('uncaughtException', error => console.log(error));
+client.connect()
 
-client.config = require('./settings/config.js');
-client.prefix = client.config.PREFIX;
-client.owner = client.config.OWNER_ID;
-
-client.distube = new DisTube(client, {
-  searchSongs: 0,
-  searchCooldown: 30,
-  leaveOnEmpty: client.config.LEAVE_EMPTY,
-  emptyCooldown: client.config.EMPTY_LEAVE,
-  leaveOnFinish: client.config.LEAVE_FINISH,
-  leaveOnStop: client.config.LEAVE_STOP,
-  plugins: [new SoundCloudPlugin(), new SpotifyPlugin()],
-  ytdlOptions: {
-    highWaterMark: 1 << 24,
-    quality: 'highestaudio'
-  },
-});
-
-["aliases", "commands"].forEach(x => client[x] = new Collection());
-["loadCommands", "loadEvents", "loadDistube"].forEach(x => require(`./handlers/${x}`)(client));
-
-
-client.login(client.config.TOKEN);
+module.exports = client; 
