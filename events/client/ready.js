c8a3f3d5f78d0ad3d6f14f565400119724fd7db6
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-09-12 14:24:19 +0700

    `Fixed` ready.js (Presence Status)

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,20 +1,20 @@
 module.exports = async (client) => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 
-  let guilds = client.guilds.cache.size;
-  let users = client.users.cache.size;
-  let channels = client.channels.cache.size;
+	let guilds = client.guilds.cache.size;
+	let users = client.users.cache.size;
+	let channels = client.channels.cache.size;
 
-  const activities = [
-      `${client.prefix}help | ${guilds} servers`,
-      `${client.prefix}play <input> | ${users} users`,
-      `${client.prefix}filterlist | ${channels} channels`,
-  ]
+	const activities = [
+		`${client.prefix}help | ${guilds} servers`,
+		`${client.prefix}play <input> | ${users} users`,
+		`${client.prefix}filterlist | ${channels} channels`,
+	];
 
     setInterval(() => {
-      client.user.setPresence({ 
-      activities: [{ name: `${activities[Math.floor(Math.random() * activities.length)]}`, type: 2 }], 
-      status: 'online', 
-    });
-  }, 15000);
+        client.user.setPresence({ 
+            activities: [{ name: `${activities[Math.floor(Math.random() * activities.length)]}`, type: 2 }], 
+            status: 'online', 
+        });
+    }, 15000)
 }
