823dda20aa486c1d56e875a5a5249b39bf24d465
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-09-27 18:46:34 +0700

    `Fixed` channelDelete.js

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-## 📑 Short Feature
+<p align="center">
+<img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&height=200&section=header&text=NanoSpacePlus&fontSize=80&fontAlignY=35&animation=twinkling&fontColor=gradient"/> </a> 
+</p>
+
+<p align="center"> 
+  <a href="https://ko-fi.com/nanotect" target="_blank"> <img src="https://ko-fi.com/img/githubbutton_sm.svg"/> </a> 
+</p>
+
+## 📑 Feature
 - [x] Music System
 - [x] Filters System
 - [x] AutoComplete (Play Commands!)
--- events/guild/channelDelete.js
+++ events/guild/channelDelete.js
@@ -6,16 +6,33 @@ module.exports = async (client, channel) => {
     if (channel.type == 2) {
         if (channel.members.has(client.user.id)) {
             const queue = client.distube.getQueue(channel.guild.id);
-            if (!queue) return;
-            client.distube.stop(channel);
-            client.distube.voices.leave(channel.guild);
+            if (queue) {
+                client.distube.stop(channel);
+                return;
+            }
+        }
+    }
+
+    if (channel.type == 13) {
+        if (channel.members.has(client.user.id)) {
+            const queue = client.distube.getQueue(channel.guild.id);
+            if (queue) {
+                client.distube.stop(channel);
+                return;
+            }
         }
     }
 
     if (channel.type == 0) {
         const db = await GSetup.get(channel.guild.id);
         if (db.setup_ch == channel.id) {
+			const queue = client.distube.getQueue(channel.guild.id);
             await client.createAlreadySetup(channel); // Can find on handlers/loadDatabase.js
+            if (queue) {
+                client.distube.stop(channel);
+                client.distube.voices.leave(channel.guild);
+                return;
+            }
         }
     }
 };
